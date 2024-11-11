6fe0abbac0766efc1f64ea572abfe24590e10c91
Tiril Marie Pedersen Løken <94623622+Tavyah@users.noreply.github.com>
2024-11-11 14:10:13 +0100

    a script to find all cisco critical vuln from 2023
    made a script that finds all the cisco critical vulnerabilities from 2023, by looping through the the files and folders

---
+++ find_all_cisco_product_vuln.py
@@ -0,0 +1,43 @@
+import filehandler_helper as fh
+
+def main() -> None:
+    regex_string = "cisco"
+    output = 'search_cisco_vuln.txt'
+    critical_string_regex = '"baseSeverity": "CRITICAL"'
+    search_for_vuln(regex_string, output, critical_string_regex)
+
+def search_for_vuln(search_word: str, output_file: str, critical_confirmed: str) -> None:
+    search_list = list_with_files()
+
+    for filepath in search_list:
+        word = False
+        critical = False
+        with open(filepath, 'r', encoding='utf-8', errors='ignore') as file:
+            for line in file:
+                if search_word in line:
+                    word = True
+                if critical_confirmed in line:
+                    critical = True
+                if word and critical:
+                    with open(output_file, 'a') as output:
+                        output.write(f"{critical_confirmed} : {filepath}\n")
+                    break
+
+def list_with_files() -> list:
+    fp = fh.get_path_of_folder('2023')
+    folders_from_folder = fh.get_folder_get_list_with_foldernames(fp)
+    list_with_folder_files = {}
+
+    for i in folders_from_folder:
+        folder_path = fh.return_filepath_joined_with_file(fp, i)
+        filename_list = fh.get_list_with_names_from_folder(fh.get_path_of_folder(folder_path))
+        list_with_folder_files[folder_path] = filename_list
+
+    list_with_all_files_to_search = []
+    for folder, file in list_with_folder_files.items():
+        for f in file:
+            list_with_all_files_to_search.append(fh.return_filepath_joined_with_file(folder, f))
+    return list_with_all_files_to_search
+
+if __name__ == "__main__":
+    main()
